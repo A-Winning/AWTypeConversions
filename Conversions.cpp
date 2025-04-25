d258bb14116788374db37ee2793bc588e8335888
Alexander Winning <alexander.r.d.winning@gmail.com>
2025-04-25 01:43:36 -0400

    Initial Files

---
+++ Conversions.cpp
@@ -0,0 +1,5 @@
+//
+// Created by alexa on 25/04/2025.
+//
+
+#include "Conversions.h"
---
+++ Conversions.h
@@ -0,0 +1,16 @@
+//
+// Created by alexa on 25/04/2025.
+//
+
+#ifndef CONVERSIONS_H
+#define CONVERSIONS_H
+
+
+
+class Conversions {
+
+};
+
+
+
+#endif //CONVERSIONS_H
