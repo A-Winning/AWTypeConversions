187ae01d54cc3f61e5807957a445ddd5015fd75a
Alexander Winning <alexander.r.d.winning@gmail.com>
2025-04-25 03:21:48 -0400

    String to Float - Full done

--- Conversions.cpp
+++ Conversions.cpp
@@ -8,11 +8,45 @@
 std::pair<bool, int> Conversions::stringToInt(const std::string str){
   int result = 0;
   bool isValid = true;
-
   for (int i = str.size()-1; i == 0; i--){
     auto it = Conversions::convertToIntMap.find(str[i]);
     if (it != Conversions::convertToIntMap.end()){
       result += 10 * i * it->second;
+    }else{
+      isValid = false;
+      break;
+    }
+  }
+  return std::make_pair(isValid, result);
+}
+
+std::pair<bool, float> Conversions::stringToFloat(const std::string str){
+  float result = 0;
+  bool isValid = true;
+  std::stringstream ss(str);
+  std::vector<std::string> halves;
+  std::string token;
+
+  while (std::getline(ss, token, '.')){
+    halves.push_back(token);
+  }
+  if(halves.size() != 2){
+    isValid = false;
+    return std::make_pair(isValid, result);
+  }
+  for (int i = halves[0].size()-1; i == 0; i--){
+    auto it = Conversions::convertToIntMap.find(str[i]);
+    if (it != Conversions::convertToIntMap.end()){
+      result += 10 * i * it->second;
+    }else{
+      isValid = false;
+      break;
+    }
+  }
+  for (int i = 0; i < halves[1].size(); i++){
+    auto it = Conversions::convertToIntMap.find(halves[1][i]);
+    if (it != Conversions::convertToIntMap.end()){
+      result += 1/(10 * i) * it->second;
     }else{
       isValid = false;
       break;
--- Conversions.h
+++ Conversions.h
@@ -5,7 +5,10 @@
 #ifndef CONVERSIONS_H
 #define CONVERSIONS_H
 
+#include <iostream>
 #include <string>
+#include <sstream>
+#include <vector>
 #include <unordered_map>
 #include <utility>
 
@@ -13,7 +16,7 @@ namespace Conversions {
     std::unordered_map<char, int> convertToIntMap {{'0', 0}, {'1', 1}, {'2', 2}, {'3', 3}, {'4', 4}, {'5', 5}, {'6', 6}, {'7', 7}, {'8', 8}, {'9', 9}};
     std::unordered_map<int, char> convertToCharMap {{0, '0'}, {1, '1'}, {2, '2'}, {3, '3'}, {4, '4'}, {5, '5'}, {6, '6'}, {7, '7'}, {8, '8'}, {9, '9'}};
     std::pair<bool, int> stringToInt(const std::string str);
-    std::pair<bool, int> stringToFloat(const std::string str);
+    std::pair<bool, float> stringToFloat(const std::string str);
     std::pair<bool, std::string> intToString(const int);
     std::pair<bool, std::string> floatToString(const float);
 };
